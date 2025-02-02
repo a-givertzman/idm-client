69620807ed300654ab0b41dba9e7af890469c36d
pivovarvalera <pivovarvaleri@gmail.com>
2025-02-02 22:06:47 +0300

    Point | Point | comments

--- lib/domain/point/point.dart
+++ lib/domain/point/point.dart
@@ -1,19 +1,18 @@
 import 'package:idm_client/domain/point/point_status.dart';
 import 'package:idm_client/domain/point/point_type.dart';
-
 ///
-/// Data unit,
-/// - name of the device
-/// - contains value
-/// - timestemp
-/// - status
+/// Class `Point` - data unit,
+/// - `name` - name of the device
+/// - `type` - defines the type
+/// - `value` - contains value
+/// - `status` - dafines the satus
+/// - `timestamp` - defines the creation time
 class Point<T> {
   final String name;
   final PointType type;
   final T value;
   final Status status;
   final String timestamp;
-
   ///
   /// Returns [Point] new instance
   const Point({
@@ -23,7 +22,8 @@ class Point<T> {
     required this.status,
     required this.timestamp,
   });
-
+  ///
+  /// Returns string representation of class content
   @override
   String toString() {
     return 'Point(name: $name, type: $type, value: $value, status: $status, timestamp: $timestamp)';
