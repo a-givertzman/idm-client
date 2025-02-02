8edba038a1d2a73c8d2f3f092885a37ea12967a9
pivovarvalera <pivovarvaleri@gmail.com>
2025-02-02 22:05:45 +0300

    Failure | comments

--- lib/domain/error/failure.dart
+++ lib/domain/error/failure.dart
@@ -1,13 +1,17 @@
 ///
-/// Error container
+/// Class `Failure` - error container
+/// - `message` - description of the error
+/// - `child` - optional child failure
 class Failure {
   final String message;
   final Failure? child;
   ///
-  /// Creates Failure new instance
+  /// Creates [Failure] new instance
+  /// - `message` - description of the error
+  /// - `child` - optional child failure, that provides additional info
   Failure(this.message, {this.child});
-  //
-  //
+  ///
+  /// Return a string representation of failure that occured
   @override
   String toString() {
     return 'Failure: $message \n\t↳$child';
