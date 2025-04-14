6c9b73b74652d76f1174fce68e451b95d2770c2d
pivovarvalera <pivovarvaleri@gmail.com>
2025-04-14 19:07:41 +0300

    Homebody | DeviceInfo | fixes

--- lib/presentation/home_page/widgets/device_info_widget.dart
+++ lib/presentation/home_page/widgets/device_info_widget.dart
@@ -1,38 +1,47 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:hmi_core/hmi_core_log.dart';
 
+///
+/// Widget for showing information frame
 class DeviceInfoWidget extends StatefulWidget {
   final String devId;
+  final VoidCallback onClosePressed;
 
+  ///
+  /// Creates a new instanse of [DeviceInfoWidget] with [key], given id of device [devId]
+  /// and callback of pressing close button
   const DeviceInfoWidget({
     super.key,
     required this.devId,
+    required this.onClosePressed,
   });
-
+  //
+  //
   @override
   State<DeviceInfoWidget> createState() => _DeviceInfoWidgetState();
 }
 
+///
+/// Status of the [DeviceInfoWidget].
 class _DeviceInfoWidgetState extends State<DeviceInfoWidget> {
   late Future<Map<String, dynamic>> _deviceInfoFuture;
   final _log = const Log("DeviceInfoWidget");
-
+  //
+  //
   @override
   void initState() {
     super.initState();
     _deviceInfoFuture = _fetchDeviceInfo(widget.devId);
   }
-
+  //
+  //
   Future<Map<String, dynamic>> _fetchDeviceInfo(String devId) async {
     try {
       await Future.delayed(const Duration(milliseconds: 300));
-
       final content = await rootBundle.loadString('assets/device/device.json');
       final devices = jsonDecode(content);
-
       Map<String, dynamic> response;
       if (devices.containsKey(devId)) {
         response = {
@@ -48,7 +57,6 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget> {
           },
         };
       }
-
       if (response['ok'] != null) {
         return devices[devId];
       } else {
@@ -59,7 +67,8 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget> {
       rethrow;
     }
   }
-
+  //
+  //
   @override
   Widget build(BuildContext context) {
     const padding = 24.0;
@@ -78,45 +87,65 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget> {
               );
             }
             if (snapshot.hasError && snapshot.data == null) {
-              return Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 34, 36, 37),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const Text('No data available for this device!',
-                      style: TextStyle(color: Colors.white)),
-                ),
-              );
+              return _buildError();
             }
-
-            final info = snapshot.data!;
-            return Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 37, 86, 123),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('ID: ${widget.devId}',
-                      style: const TextStyle(color: Colors.white)),
-                  Text('manufacturer: ${info['manufacturer']}',
-                      style: const TextStyle(color: Colors.white)),
-                  Text('name: ${info['name']}',
-                      style: const TextStyle(color: Colors.white)),
-                  Text('model: ${info['model']}',
-                      style: const TextStyle(color: Colors.white)),
-                  Text('description: ${info['description']}',
-                      style: const TextStyle(color: Colors.white)),
-                ],
-              ),
-            );
+            return _buildInfo(snapshot);
           },
         ),
       ),
     );
+  }
+
+  ///
+  /// Building an error frame.
+  Widget _buildError() {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 34, 36, 37),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Text('No data available for this device!',
+            style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  ///
+  /// Building an inforamation frame.
+  Widget _buildInfo(AsyncSnapshot<Map<String, dynamic>> snapshot) {
+    final info = snapshot.data!;
+    return Stack(children: [
+      Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 37, 86, 123),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('ID: ${widget.devId}',
+                style: const TextStyle(color: Colors.white)),
+            Text('manufacturer: ${info['manufacturer']}',
+                style: const TextStyle(color: Colors.white)),
+            Text('name: ${info['name']}',
+                style: const TextStyle(color: Colors.white)),
+            Text('model: ${info['model']}',
+                style: const TextStyle(color: Colors.white)),
+            Text('description: ${info['description']}',
+                style: const TextStyle(color: Colors.white)),
+          ],
+        ),
+      ),
+      Positioned(
+          top: 6,
+          right: 6,
+          child: IconButton(
+              color: Colors.white,
+              onPressed: widget.onClosePressed,
+              icon: const Icon(Icons.close)))
+    ]);
   }
 }
